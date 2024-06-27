517e041f3d1c5e02255bd70aa220e03be1838793
Nicherke <nicksoncheruiyot001@gmail.com>
2024-06-27 18:20:55 +0300

    java boolean number

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //String txt = scan.next(); //string
+        //Double txt = scan.nextDouble();// double
+        boolean txt = scan.nextBoolean();//boolean// true or false
+
+        System.out.println(txt);
+    }
+}
